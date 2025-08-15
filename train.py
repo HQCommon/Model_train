0fc958fe382c332f421d37b4f21a16f90cae642e
Thyler Cruz <Thylercruz@outlook.com>
2025-08-15 01:17:01 -0500

    for pi

--- train.py
+++ train.py
@@ -1,23 +1,36 @@
 from ultralytics import YOLO
-import numpy as np
 import cv2 as cv
 from utils import video_Inf, img_Inf
+# import torch
 
 # Load a model
-model = YOLO("yolo11n.yaml")  # build a new model from YAML
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
+# model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+# model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
+
+# torch.cuda.empty_cache()
 
 # Train the model
-results = model.train(data="D:\\VSC\\Test_train\\use_data\\data.yaml", epochs=100, imgsz=800)
+# if __name__ == '__main__':
+#     results = model.train(data="D:\\VSC\\Test_train\\Weapon_Datasets\\Handgun_with_LongGun_surv\\v2\\data.yaml", epochs=200, imgsz=1080, batch=.7, device=0, patience=50)
 
-metrics = model.val(data="D:\\VSC\\Test_train\\use_data\\data.yaml", plots=True)
-print(metrics.confusion_matrix.to_df())
+# if __name__ == '__main__':
+#     model = YOLO("D:\\VSC\\Test_train\\runs\\detect\\train\\weights\\last.pt")
+#     results = model.train(resume=True, batch=.07, device=0, patience=50)
+
+# ------------------------Load trained model------------------------------
+model = YOLO("D:\\VSC\\Test_train\\runs\\detect\\train\\weights\\best.pt") # CHANGE THIS FOR PI
+# model = YOLO("D:\\VSC\\Test_train\\runs\\detect\\Hanguns\\Handguns_with_LongGuns\\v2\\940x940\\train_200epoch\\weights\\best.pt")
+
+source_vid = 0 #"D:\\VSC\\Test_train\\Vids\\short.mp4"
+# source_pic = "D:\\VSC\\Test_train\\Pics\\IMG_2690-scaled.jpeg"
+
+# ------IMAGES OR VIDEOS------
+# img_Inf(model, source_pic)
+video_Inf(model, source_vid)
+# model.predict(source, save=False, imgsz=640, conf=0.4, show=True)
 
 
-#            RETRAINING
-# Load the partially trained model
-# model = YOLO("path/to/last.pt")
+# -------EXPORTING--------
+# model.export(format="ncnn", device=0, data="D:\\VSC\\Test_train\\Weapon_Datasets\\Handgun_with_LongGun_surv\\v2\\data.yaml", imgsz=940)
+# model.export(format="openvino", device=0, data="D:\\VSC\\Test_train\\Datasets_to_use\\6kCars\\920x920\\data.yaml", imgsz=928)
 
-# Resume training
-# results = model.train(resume=True)
--- utils.py
+++ utils.py
@@ -2,44 +2,61 @@ import cv2 as cv
 from ultralytics import YOLO
 
 # FOR IMAGES
-def img_Inf(results, image):
+def img_Inf(model_use, image):
+    model = YOLO(model_use)
+    
+    image = cv.imread(image)
+    results = model(image)
+
+    if len(results[0].boxes) == 0:
+        cv.imshow('No Detections', image)
+        print("NO DETECTIONS")
+        cv.waitKey(0)
+        return
+
     for r in results:
         for i, box in enumerate(r.boxes):
-            cords = box.xyxy.numpy().astype(int).tolist()
+            class_idx = box.cls
+            cords = box.xyxy.cpu().numpy().astype(int).tolist()
 
             if len(cords) == 1:
                 pointer = cords[0]
             x1, y1, x2, y2 = pointer
 
-            print(f'x1 = {x1}, y1 = {y1}, x2 = {x2}, y2 = {y2}')
+            # print(f'x1 = {x1}, y1 = {y1}, x2 = {x2}, y2 = {y2}')
+            print(f'{model.names[int(class_idx)]} = {round(float(box.conf), 3)}')
 
-            cv.putText(image, f'Person {i}', (x1, y1 - 5), cv.FONT_HERSHEY_TRIPLEX, .5, (255,255,255), thickness= 1)
+            cv.putText(image, f"{model.names[int(class_idx)]} || {round(float(box.conf), 3)}", (x1, y1 - 5), cv.FONT_HERSHEY_TRIPLEX, .5, (255,255,255), thickness= 1)
             cv.rectangle(image, (x1,y1), (x2,y2), (0,250,0), thickness=2)
-            cv.imshow('Person Detect', image)
+            cv.imshow('Image', image)
 
     cv.waitKey(0)
 
 # FOR VIDEOS
-def video_Inf(model_use,video_path):
+def video_Inf(model_use, video_path):
+    model = YOLO(model_use)
     capture = cv.VideoCapture(video_path)
 
     while True:
         isTrue, frame = capture.read()
-        results = model_use(frame)
+        results = model(frame)
+
+        if len(results[0].boxes) == 0:
+            cv.imshow('Video', frame)
 
         for r in results:
             for i, box in enumerate(r.boxes):
-                cords = box.xyxy.numpy().astype(int).tolist()
+                class_idx = box.cls
+                cords = box.xyxy.cpu().numpy().astype(int).tolist()
 
                 if len(cords) == 1:
                     pointer = cords[0]
                 x1, y1, x2, y2 = pointer
 
-                print(f'x1 = {x1}, y1 = {y1}, x2 = {x2}, y2 = {y2}')
-
-                cv.putText(frame, f'Person {i}', (x1, y1 - 5), cv.FONT_HERSHEY_TRIPLEX, .5, (255,255,255), thickness= 1)
+                # print(f'x1 = {x1}, y1 = {y1}, x2 = {x2}, y2 = {y2}')
+                cv.putText(frame, f"{model.names[int(class_idx)]} || {round(float(box.conf), 3)}", (x1, y1 - 5), cv.FONT_HERSHEY_TRIPLEX, .5, (255,0,0), thickness= 1)
                 cv.rectangle(frame, (x1,y1), (x2,y2), (0,250,0), thickness=2)
-                cv.imshow('Person Detect', frame)
+                cv.imshow('Video', frame)
 
         if cv.waitKey(20) & 0xFF==ord('d'):
             break
